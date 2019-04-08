9791c6be3dede933ae3e24af7ba0edd7003baa30
mlego <mlegodev@gmail.com>
2019-04-07 21:19:59 -0700

    Hooked up Model to ModelView for mac target

--- SlideShow-mac/AppDelegate.swift
+++ SlideShow-mac/AppDelegate.swift
@@ -7,6 +7,7 @@ class AppDelegate: NSObject, NSApplicationDelegate {
 
     func applicationDidFinishLaunching(_ aNotification: Notification) {
         
+        NSApplication.shared.keyWindow?.setFrame(NSRect(x: 0, y: 0, width: 800, height: 800), display: true )
         NSApplication.shared.keyWindow?.center()
         
         if let controller = NSApplication.shared.keyWindow?.contentViewController as? ViewController {
--- SlideShow-mac/ViewController.swift
+++ SlideShow-mac/ViewController.swift
@@ -3,8 +3,31 @@
 import Cocoa
 import ShowEngine
 
+class Model {
+    let photographer: String
+    let location: String
+    let likes: String
+    let image: NSImage
+    
+    init(from model: ShowEngineModel) {
+        photographer = "Photographer: \(model.user.name ?? " Unknown")"
+        location = "Location: \(model.user.location ?? "Anywhere")"
+        likes = "Likes: \(model.likes ?? 0)"
+        
+        if let imageData = model.imageData,
+            let anImage = NSImage(data: imageData) {
+            image = anImage
+        } else {
+            image = NSImage(named: "sadFace") ?? NSImage()
+        }
+    }
+}
+
 class ViewModel {
     let image = Observable<NSImage>(value: NSImage())
+    let location = Observable<String>(value: String())
+    let likes = Observable<String>(value: String())
+    let photographer = Observable<String>(value: String())
 }
 
 class ViewController: NSViewController, ShowEngineOutput {
@@ -18,24 +41,41 @@ class ViewController: NSViewController, ShowEngineOutput {
         super.viewDidLoad()
         
         viewModel = ViewModel()
-        viewModel?.image.addObserver { [weak self] image in
-            self?.outputImageView.image = image
+        viewModel?.image.addObserver { [weak self] in
+            self?.outputImageView.image = $0
         }
+        
+//        viewModel?.location.addObserver { [weak self] in
+//            self?.locationLabel.text = $0
+//        }
+//
+//        viewModel?.likes.addObserver { [weak self] in
+//            self?.likesLabel.text = $0
+//        }
+//
+//        viewModel?.photographer.addObserver { [weak self] in
+//            self?.photographerLabel.text = $0
+//        }
     }
     
     public func imageLoadSuccess(data: ShowEngineModel) {
-        if let imageData = data.imageData,
-            let image = NSImage(data: imageData) {
-            viewModel?.image.value = image
-        }
+        let myModel = Model(from: data)
+        viewModel?.image.value = myModel.image
+        viewModel?.location.value = myModel.location
+        viewModel?.likes.value = myModel.likes
+        viewModel?.photographer.value = myModel.photographer
     }
     
     public func imageLoadFailure() {
-        
+        viewModel?.image.value = NSImage(named: "sadFace") ?? NSImage()
     }
     
     func start() {
         showEngine?.start()
+    }
+    
+    func stop() {
+        showEngine?.stop()
     }
 
     override var representedObject: Any? {
