7d05a875a2aafdc473fce843f6d4c1e6bd920d92
mlego <mlegodev@gmail.com>
2019-04-08 16:03:28 -0700

    removed unused public header import

--- ShowEngineFramework/ShowEngine-iOS/ShowEngine_iOS.h
+++ ShowEngineFramework/ShowEngine-iOS/ShowEngine_iOS.h
@@ -10,5 +10,3 @@ FOUNDATION_EXPORT const unsigned char ShowEngine_iOSVersionString[];
 
 // In this header, you should import all the public headers of your framework using statements like #import <ShowEngine_iOS/PublicHeader.h>
 
-<ShowEngine_iOS/ShowEngineOutput.h>
-
