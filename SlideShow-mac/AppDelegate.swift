8a51e483ac292c76a9e1fefeac37c42d3c9aeff4
mlego <mlegodev@gmail.com>
2019-04-07 20:39:24 -0700

    For Mac target, window is now centered on screen, images don't resize window

--- SlideShow-mac/AppDelegate.swift
+++ SlideShow-mac/AppDelegate.swift
@@ -7,7 +7,10 @@ class AppDelegate: NSObject, NSApplicationDelegate {
 
     func applicationDidFinishLaunching(_ aNotification: Notification) {
         
+        NSApplication.shared.keyWindow?.center()
+        
         if let controller = NSApplication.shared.keyWindow?.contentViewController as? ViewController {
+
             let useCase = ShowEngineUseCaseFactory().makeUseCase(output: controller)
             controller.showEngine = useCase
             controller.start()
